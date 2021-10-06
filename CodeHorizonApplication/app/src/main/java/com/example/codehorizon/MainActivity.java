7c3cac7068a7d026f98c7058fd5b27a93296860f
devonDunham <itisdevondunham@gmail.com>
2021-10-05 21:55:07 -0400

    Initialization of THE app
    Intial file of Code Horizon application

---
+++ CodeHorizonApplication/app/src/main/java/com/example/codehorizon/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.codehorizon;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void clickFunction(View v){
+        Log.i("Button", "Button Clicked");
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
