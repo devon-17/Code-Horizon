5bac896741eae350106ff0689caba2daa1b57668
devonDunham <itisdevondunham@gmail.com>
2021-10-13 22:04:53 -0400

    Failed Parse install

--- CodeHorizonApplication/app/src/main/java/com/example/codehorizon/MainActivity.java
+++ CodeHorizonApplication/app/src/main/java/com/example/codehorizon/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import com.parse.Parse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,5 +17,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ParseObject firstObject = new  ParseObject("FirstClass");
+        firstObject.put("message","Hey ! First message from android. Parse is now connected");
+        firstObject.saveInBackground(e -> {
+            if (e != null){
+                Log.e("MainActivity", e.getLocalizedMessage());
+            }else{
+                Log.d("MainActivity","Object saved.");
+            }
+        });
     }
 }
