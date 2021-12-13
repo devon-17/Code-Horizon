a716c699caabcd0fc1f33f44fa2c2b4ec0a1aadf
devonDunham <itisdevondunham@gmail.com>
2021-12-12 19:42:35 -0500

    ongoing

--- Website/dropdown.js
+++ Website/dropdown.js
@@ -1,10 +1,6 @@
-function test(option) {
+function changeCodeType(option) {
 
-    var if_text = document.getElementById('if_statement_content');
-    var if_code_block = "when x happens something else happens";
-    var if_syntax = "if (x) { y; }"
-    
-
+    var if_text = document.getElementById('ifStatementContent');
     var selected = (option.options[option.selectedIndex].text);
     // alert("You Chose: " + selected);
 
@@ -16,9 +12,10 @@ function test(option) {
     }
     */
 
-    
+    var if_code_block = "when x happens something else happens";
+    var if_syntax = "if (x) { y; }";
     // change to proper text
-    if (selected == "< Syntax Highlighting />") {
+    if (selected == "< Syntax />") {
         if_text.innerHTML = if_syntax;
     } else if (selected == "< Block Code />") {
         if_text.innerHTML = if_code_block;
