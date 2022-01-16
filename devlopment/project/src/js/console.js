88649efa54f595fcd9289fd40e5d1b46d734c2d3
Devon Dunham <itisdevondunham@gmail.com>
2022-01-16 10:40:56 -0500

    ongoing: cards

---
+++ devlopment/project/src/js/console.js
@@ -0,0 +1,6 @@
+document.addEventListener("keypress", function (e) {
+    var keyPressed = e.key;
+    if (keyPressed == "r" || "R") {
+      alert("You pressed the 'R' key");
+    }
+  })
--- devlopment/project/src/js/draggable.js
+++ devlopment/project/src/js/draggable.js
@@ -40,23 +40,50 @@ function init() {
     for (var d in diagram) {
       var node = diagram[d];
 
-      var ifStatement =
-        '<div class="moveable-element">' +
-        '<div class="moveable-element-header orange">IF STATEMENT</div>' +
-        '<p class="if-statement-content">if something happens then something else happens</p>' +
-        "</div>";
+      var ifStatement = '<div class="card moveable-element" style="width: 18rem;">' +
+        "<img class='card-img-top' src='https://lh6.googleusercontent.com/mh-T176s3Uy_78QMm8mtDcbYYycTyCad4eFQW2iP4mUDvcMq1aQRhLtUx0EZSGU1LqQvpf5XK4P7M8zQqrCs-QwS2l0vGBoJHPafRit_lSbSIEzU18SEAAfJTo9kZ13Bpdjia8pM' alt='Card image cap'>" +
+        '<div class="card-body">' +
+        '<h5 class="card-title">IF STATEMENT</h5>' +
+        '<p class="card-text if-statement-content">if something happens then something else happens</p>' +
+        '<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else" target="_blank" class="btn btn-outline-dark">More Info</a>' +
+        '</div>'
+      '</div>';
 
-      var variables =
-        '<div class="moveable-element">' +
-        '<div class="moveable-element-header purple">VARIABLES</div>' +
-        '<p class="variables-content">A way to easily store data with a semantic name of your choosing</p>' +
-        "</div>";
+      // var ifStatement =
+      //   '<div class="moveable-element">' +
+      //   '<div class="moveable-element-header orange">IF STATEMENT</div>' +
+      //   '<p class="if-statement-content">if something happens then something else happens</p>' +
+      //   "</div>";
 
-      var forLoops =
-        '<div class="moveable-element">' +
-        '<div class="moveable-element-header red-pink">FOR LOOPS</div>' +
-        '<p class="for-loop-content">A way to easily specify and control iteration</p>' +
-        "</div>";
+      var variables = '<div class="card moveable-element" style="width: 18rem;">' +
+        "<img class='card-img-top' src='https://miro.medium.com/max/1400/1*MGV0wog7EzunCowymzE3Xg.png' alt='Card image cap'>" +
+        '<div class="card-body">' +
+        '<h5 class="card-title">VARIABLES</h5>' +
+        '<p class="card-text variables-content">A way to easily store data with a semantic name of your choosing</p>' +
+        '<a href="https://launchschool.com/books/ruby/read/variables" target="_blank" class="btn btn-outline-dark">More Info</a>' +
+        '</div>'
+      '</div>';
+
+      // var variables =
+      //   '<div class="moveable-element">' +
+      //   '<div class="moveable-element-header purple">VARIABLES</div>' +
+      //   '<p class="variables-content">A way to easily store data with a semantic name of your choosing</p>' +
+      //   "</div>";
+
+    var forLoops = '<div class="card moveable-element" style="width: 18rem;">' +
+        "<img class='card-img-top' src='https://miro.medium.com/max/1400/1*MGV0wog7EzunCowymzE3Xg.png' alt='Card image cap'>" +
+        '<div class="card-body">' +
+        '<h5 class="card-title">FOR LOOPS</h5>' +
+        '<p class="card-text for-loop-content">A way to easily specify and control iteration</p>' +
+        '<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration" target="_blank" class="btn btn-outline-dark">More Info</a>' +
+        '</div>'
+      '</div>';
+
+      // var forLoops =
+      //   '<div class="moveable-element">' +
+      //   '<div class="moveable-element-header red-pink">FOR LOOPS</div>' +
+      //   '<p class="for-loop-content">A way to easily specify and control iteration</p>' +
+      //   "</div>";
 
       var html = "";
       if (node.type === "ELEMENT-1") {
