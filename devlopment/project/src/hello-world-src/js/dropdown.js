5e7df07424aa08bc1b5d58c075f3c773c6975c1d
Devon Dunham <itisdevondunham@gmail.com>
2022-01-26 08:23:52 -0500

    ongoing: hello world

--- devlopment/project/src/hello-world-src/js/dropdown.js
+++ devlopment/project/src/hello-world-src/js/dropdown.js
@@ -7,9 +7,10 @@ function init() {
     // $("#tableButton").text(selText); //Using Jquery
 
     var consoleContent = $(".console-statement-content");
+    var consoleSyntaxInput =
+      '<p>console.log(<input type="text" class="var-console-name-input" placeholder="var name">)</p>';
 
     var varContent = $(".var-content");
-
     var varSyntaxInput =
       '<p>var <input type="text" class="var-syntax-name-input" placeholder="variable name"><input type="text" class="var-syntax-input" placeholder="variable msg"></p>';
 
@@ -21,8 +22,8 @@ function init() {
         "A way to easily store data with a semantic name of your choosing"
       );
     } else if (selected == "Syntax") {
-      consoleContent.text("console.log(msg);");
-      varContent.text(varSyntaxInput);
+      consoleContent.html(consoleSyntaxInput);
+      varContent.html(varSyntaxInput);
     } else return;
   });
 }
