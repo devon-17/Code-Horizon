e349382a388c0a9e81cc2c70cd071b6d201331e0
devonDunham <itisdevondunham@gmail.com>
2022-01-30 19:32:14 -0500

    ongoing: hello world

--- devlopment/project/src/hello-world-src/js/console.js
+++ devlopment/project/src/hello-world-src/js/console.js
@@ -15,13 +15,14 @@ function getInputValue() {
   var varString = document.getElementById("var-string").value;
   var consoleVarName = document.getElementById("console-var-name").value;
 
-  var consoleOutputMsg = varString + "<br>";
+  var consoleOutputMsg =
+    "<p class='output'>Object: " + varString + "</p>" + "<br>";
   var index = 0;
 
   if (varName && consoleVarName != "") {
     // Displaying the value
     if (varName == consoleVarName) {
-      consoleOutput.append("Object: " + consoleOutputMsg);
+      consoleOutput.append(consoleOutputMsg);
       index += 1;
     } else {
       alert("There is an error somewhere, Check var names");
