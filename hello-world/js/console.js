a2c469078d85b0f3f326f850662f3eefe50932c5
Devon Dunham <itisdevondunham@gmail.com>
2023-05-07 12:47:19 -0400

    HTML

---
+++ hello-world/js/console.js
@@ -0,0 +1,34 @@
+// $(document).ready(function () {
+//   // Get value on button click and show alert
+//   $("#console-btn").click(function () {
+//     var str = $(".var-syntax-name-input").attr("value");
+//     alert(str);
+//   });
+// });
+
+function getInputValue() {
+  var consoleOutput = $(".console");
+  // const console = document.getElementsByClassName("console");
+
+  // Selecting the input element and get its value
+  var varName = document.getElementById("var-name").value;
+  var varString = document.getElementById("var-string").value;
+  var consoleVarName = document.getElementById("console-var-name").value;
+
+  var consoleOutputMsg =
+    "<p class='output'>Object: " + varString + "</p>" + "<br>";
+  var index = 0;
+
+  if (varName && consoleVarName != "") {
+    // Displaying the value
+    if (varName == consoleVarName) {
+      consoleOutput.append(consoleOutputMsg);
+      index += 1;
+    } else {
+      alert("There is an error somewhere, Check var names");
+    }
+  } else {
+    alert("Please fill in fields");
+  }
+  console.log(index);
+}
---
+++ home/js/draggable.js
@@ -0,0 +1,113 @@
+$(init);
+function init() {
+  var diagram = [];
+  var canvas = $(".canvas");
+
+  $(".inventory-element").draggable({
+    helper: "clone",
+  });
+  canvas.droppable({
+    drop: function (event, ui) {
+      var node = {
+        _id: new Date().getTime(),
+        position: ui.helper.position(),
+      };
+      node.position.left -= canvas.position().left;
+      if (ui.helper.hasClass("element-1")) {
+        node.type = "ELEMENT-1";
+      } else if (ui.helper.hasClass("element-2")) {
+        node.type = "ELEMENT-2";
+      } else if (ui.helper.hasClass("element-3")) {
+        node.type = "ELEMENT-3";
+      } else {
+        return;
+      }
+      diagram.push(node);
+      renderDiagram(diagram);
+    },
+  });
+  function renderDiagram(diagram) {
+    var canvasTitle = "<h2 class='title-text'>Code Canvas</h2>";
+
+    canvas.empty();
+    canvas.append(canvasTitle);
+
+    for (var d in diagram) {
+      var node = diagram[d];
+
+      var ifStatement =
+        '<div class="card moveable-element" style="width: 18rem; min-height: 10rem;">' +
+        '<div class="card-body">' +
+        '<h5 class="card-title">IF STATEMENT</h5>' +
+        '<p class="card-text if-statement-content">if something happens then something else happens</p>' +
+        '<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/if...else" target="_blank" class="btn btn-outline-dark">More Info</a>' +
+        "</div>";
+      ("</div>");
+
+      // var ifStatement =
+      //   '<div class="moveable-element">' +
+      //   '<div class="moveable-element-header orange">IF STATEMENT</div>' +
+      //   '<p class="if-statement-content">if something happens then something else happens</p>' +
+      //   "</div>";
+
+      var variables =
+        '<div class="card moveable-element" style="width: 18rem; min-height: 10rem;">' +
+        '<div class="card-body">' +
+        '<h5 class="card-title">VARIABLES</h5>' +
+        '<p class="card-text variables-content">A way to easily store data with a semantic name of your choosing</p>' +
+        '<a href="https://launchschool.com/books/ruby/read/variables" target="_blank" class="btn btn-outline-dark">More Info</a>' +
+        "</div>";
+      ("</div>");
+
+      // var variables =
+      //   '<div class="moveable-element">' +
+      //   '<div class="moveable-element-header purple">VARIABLES</div>' +
+      //   '<p class="variables-content">A way to easily store data with a semantic name of your choosing</p>' +
+      //   "</div>";
+
+      var forLoops =
+        '<div class="card moveable-element" style="width: 18rem; min-height: 10rem;">' +
+        '<div class="card-body">' +
+        '<h5 class="card-title">FOR LOOPS</h5>' +
+        '<p class="card-text for-loop-content">A way to easily specify and control iteration</p>' +
+        '<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Loops_and_iteration" target="_blank" class="btn btn-outline-dark">More Info</a>' +
+        "</div>";
+      ("</div>");
+
+      // var forLoops =
+      //   '<div class="moveable-element">' +
+      //   '<div class="moveable-element-header red-pink">FOR LOOPS</div>' +
+      //   '<p class="for-loop-content">A way to easily specify and control iteration</p>' +
+      //   "</div>";
+
+      var html = "";
+      if (node.type === "ELEMENT-1") {
+        html = ifStatement;
+      } else if (node.type === "ELEMENT-2") {
+        html = variables;
+      } else if (node.type === "ELEMENT-3") {
+        html = forLoops;
+      }
+      var dom = $(html)
+        .css({
+          position: "absolute",
+          top: node.position.top,
+          left: node.position.left,
+        })
+        .draggable({
+          stop: function (event, ui) {
+            console.log(ui);
+            var id = ui.helper.attr("id");
+            for (var i in diagram) {
+              if (diagram[i]._id == id) {
+                diagram[i].position.top = ui.position.top;
+                diagram[i].position.left = ui.position.left;
+              }
+            }
+          },
+        })
+        .attr("id", node._id);
+      canvas.append(dom);
+    }
+  }
+}
